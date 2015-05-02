fba7398953d312c5f09cf38fc616a020c5c49869
Flavius12 <info@evolsoft.tk>
2015-05-02 09:54:49 +0200

    Update phartools.sh

--- phartools.sh
+++ phartools.sh
@@ -3,7 +3,7 @@ if type php >/dev/null 2>&1; then #Check if PHP is installed
     php phartools.php $@
 else
     while true; do
-        read -p "PHP is not installed! Do you want to install it now (y, n)?" yn
+        read -p "PHP is not installed! Do you want to install it now (y, n)? " yn
         case $yn in
             [Yy]* ) sudo apt-get install php5-cli; sudo sed -i.bak 's/^;phar.readonly/phar.readonly/' /etc/php5/cli/php.ini; sudo sed -i.bak 's/phar.readonly = On/phar.readonly = 0/' /etc/php5/cli/php.ini; exit;; #based on default php.ini config (by default ;phar.readonly = 0 or phar.readonly = On)
             [Nn]* ) echo "PHP installation cancelled"; exit;;
