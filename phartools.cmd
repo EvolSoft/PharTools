e8a3293fe6e6010a94bb9736109bd996b04a7c37
Flavius12 <info@evolsoft.tk>
2018-04-04 14:21:54 +0200

    Fixed Windows batch script

--- phartools.cmd
+++ phartools.cmd
@@ -4,7 +4,7 @@
 set PHP_PATH=%~dp0\bin\php\php.exe
 
 if exist "%PHP_PATH%" (
-    "%PHP_PATH%" %~dp0\phartools.php %*
+    "%PHP_PATH%" "%~dp0\phartools.php" %*
 ) else (
     echo PHP Executable not found. Please set the right PHP path.
 )
